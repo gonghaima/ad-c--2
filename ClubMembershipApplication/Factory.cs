c742af2e156d39c8a5e1b88e3b8eb17f79d37a6a
Steven Gong <gonghaima@hotmail.com>
2024-01-04 00:58:34 +0900

    code first - dotnet ef migrations add FirstCreate

---
+++ ClubMembershipApplication/Factory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ClubMembershipApplication.FieldValidators;
+using ClubMembershipApplication.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using ClubMembershipApplication.FieldValidators;
+
+namespace ClubMembershipApplication
+{
+    public static class Factory
+    {
+        
+    }
+}
+
